04593670603cbe0ba4b13d3345744829e6d602aa
夏韵凯 <yjyxiayk@sunnyoptical.com>
2021-06-25 17:37:33 +0800

    新增Python LQR

---
+++ Python/draw/drawCar.py
@@ -0,0 +1,113 @@
+import numpy as np
+import matplotlib.pyplot as plt
+PI = np.pi
+# Vehicle config
+wheelbase = 2.33  # wheel base: front to rear axle [m]
+wheeldist = 1.85  # wheel dist: left to right wheel [m]
+v_w = 2.33  # vehicle width [m]
+r_b = 0.80  # rear to back [m]
+r_f = 3.15  # rear to front [m]
+t_r = 0.40  # tire radius [m]
+t_w = 0.30  # tire width [m]
+
+c_f = 155494.663  # [N / rad]
+c_r = 155494.663  # [N / rad]
+m_f = 570  # [kg]
+m_r = 570  # [kg]
+l_f = 1.165  # [m]
+l_r = 1.165  # [m]
+Iz = 1436.24  # [kg m2]
+
+# Controller Config
+ts = 0.10  # [s]
+max_iteration = 150
+eps = 0.01
+
+matrix_q = [1.0, 0.0, 1.0, 0.0]
+matrix_r = [1.0]
+
+state_size = 4
+
+max_acceleration = 5.0  # [m / s^2]
+max_steer_angle = np.deg2rad(40)  # [rad]
+max_speed = 30 / 3.6  # [km/h]
+class Arrow:
+    def __init__(self, x, y, theta, L, c):
+        angle = np.deg2rad(30)
+        d = 0.3 * L
+        w = 2
+
+        x_start = x
+        y_start = y
+        x_end = x + L * np.cos(theta)
+        y_end = y + L * np.sin(theta)
+
+        theta_hat_L = theta + PI - angle
+        theta_hat_R = theta + PI + angle
+
+        x_hat_start = x_end
+        x_hat_end_L = x_hat_start + d * np.cos(theta_hat_L)
+        x_hat_end_R = x_hat_start + d * np.cos(theta_hat_R)
+
+        y_hat_start = y_end
+        y_hat_end_L = y_hat_start + d * np.sin(theta_hat_L)
+        y_hat_end_R = y_hat_start + d * np.sin(theta_hat_R)
+
+        plt.plot([x_start, x_end], [y_start, y_end], color=c, linewidth=w)
+        plt.plot([x_hat_start, x_hat_end_L],
+                 [y_hat_start, y_hat_end_L], color=c, linewidth=w)
+        plt.plot([x_hat_start, x_hat_end_R],
+                 [y_hat_start, y_hat_end_R], color=c, linewidth=w)
+
+
+def draw_car(x, y, yaw, steer, color='black'):
+    car = np.array([[-r_b, -r_b, r_f, r_f, -r_b],
+                    [v_w / 2, -v_w / 2, -v_w / 2, v_w / 2, v_w / 2]])
+
+    wheel = np.array([[-t_r, -t_r, t_r, t_r, -t_r],
+                      [t_w / 2, -t_w / 2, -t_w / 2, t_w / 2, t_w / 2]])
+
+    rlWheel = wheel.copy()
+    rrWheel = wheel.copy()
+    frWheel = wheel.copy()
+    flWheel = wheel.copy()
+
+    Rot1 = np.array([[np.cos(yaw), -np.sin(yaw)],
+                     [np.sin(yaw), np.cos(yaw)]])
+
+    Rot2 = np.array([[np.cos(steer), np.sin(steer)],
+                     [-np.sin(steer), np.cos(steer)]])
+
+    frWheel = np.dot(Rot2, frWheel)
+    flWheel = np.dot(Rot2, flWheel)
+
+    frWheel += np.array([[wheelbase], [-wheeldist / 2]])
+    flWheel += np.array([[wheelbase], [wheeldist / 2]])
+    rrWheel[1, :] -= wheeldist / 2
+    rlWheel[1, :] += wheeldist / 2
+
+    frWheel = np.dot(Rot1, frWheel)
+    flWheel = np.dot(Rot1, flWheel)
+
+    rrWheel = np.dot(Rot1, rrWheel)
+    rlWheel = np.dot(Rot1, rlWheel)
+    car = np.dot(Rot1, car)
+
+    frWheel += np.array([[x], [y]])
+    flWheel += np.array([[x], [y]])
+    rrWheel += np.array([[x], [y]])
+    rlWheel += np.array([[x], [y]])
+    car += np.array([[x], [y]])
+
+    plt.plot(car[0, :], car[1, :], color)
+    plt.plot(frWheel[0, :], frWheel[1, :], color)
+    plt.plot(rrWheel[0, :], rrWheel[1, :], color)
+    plt.plot(flWheel[0, :], flWheel[1, :], color)
+    plt.plot(rlWheel[0, :], rlWheel[1, :], color)
+    Arrow(x, y, yaw, 0.8 * wheelbase, color)
+    plt.axis("equal")
+    # plt.show()
+
+
+if __name__ == '__main__':
+    draw_car(0, 0, 0, 0.2)
--- Python/pathTracking/LQR_steer_speed.py
+++ Python/pathTracking/LQR_steer_speed.py
@@ -5,10 +5,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 sys.path.append("../curves/")
+sys.path.append("../draw/")
 try:
     import cubic_spline_planner
+    import draw.drawCar as draw
 except ImportError:
     raise
+"""
+A=[
+    [1, 0, -T * v *sin(yaw)],
+    [0, 1, T * V * cos(yaw)],
+    [0, 0, 1]
+]
+
+B = [
+     [T * cos(yaw), 0],
+     [T * sin(yaw), 0],
+     [T * tan(yaw) / L, T * v /(L * (cos(yaw)^2)]
+    ]
+    X(k+1) = AX(K)+Bu(k)
+"""
+
+
+
+def pi_2_pi(angle):
+    return (angle + math.pi) % (2 * math.pi) - math.pi
 def calc_target_index(pos_x,pos_y, refPos_x,refPos_y):
     dist=[]
     for i in range(0, len(refPos_x)):
@@ -19,38 +40,139 @@ def calc_target_index(pos_x,pos_y, refPos_x,refPos_y):
     mind = min(dist)
     return dist.index(mind)
 
+def calcu_K(A,B,Q,R):
+    iter_max = 5000
+    epsilon = 0.001
+    P_old = Q
+    AT = A.T
+    BT = B.T
+    for i in range(iter_max):
+        P_new = AT @ P_old @ A - AT @ P_old @ B @ np.linalg.inv(R + BT @ P_old @ B) @ (BT @ P_old @ A) + Q
+        if (abs(P_new - P_old)).max() < epsilon:
+            break
+        else:
+            P_old = P_new
+        P = P_new
+    K = np.linalg.inv(BT @ P @ B + R) @ (BT @ P @ A)
+    return K
+
+def LQR_contorl(idx,x,y,v,yaw,refPos_x,refPos_y,refPos_yaw,refPos_Delta,refK,L,Q,R,dt):
+    x_r = refPos_x[idx]
+    y_r = refPos_y[idx]
+    heading_r = refPos_yaw[idx]
+    delta_r = math.atan2(refK[idx]*L, 1)
+    x_error  = x - x_r
+    y_error = y - y_r
+    yaw_error =  yaw - heading_r
+    latError = y_error*math.cos(heading_r) - x_error*math.sin(heading_r)
+
+    X = np.zeros((3, 1))
+    X[0, 0] = x_error
+    X[1, 0] = y_error
+    X[2, 0] = yaw_error
+
+    #由状态方程矩阵系数，计算K
+    """
+A=[
+    [1, 0, -T * v *sin(yaw)],
+    [0, 1, T * V * cos(yaw)],
+    [0, 0, 1]
+]
+
+B = [
+     [T * cos(yaw), 0],
+     [T * sin(yaw), 0],
+     [T * tan(yaw) / L, T * v /(L * (cos(yaw)^2)]
+    ]
+    X(k+1) = AX(K)+Bu(k)
+"""
+    A = np.zeros((3, 3))
+    A[0, 0] = 1
+    A[0, 2] = -v* dt * math.sin(heading_r)
+    A[1, 1] = 1
+    A[1, 2] = v * dt * math.cos(heading_r)
+    A[2, 2] = 1
+
+    B = np.zeros((3, 2))
+    B[0, 0] = dt * math.cos(heading_r)
+    B[1, 0] = dt * math.sin(heading_r)
+    B[2, 0] =  dt * math.tan(heading_r)/L
+    B[2, 1] = v*dt/(L * (math.pow(math.cos(delta_r),2)))
+    """
+    A = [1,  0,  -v*dt*sin(heading_r);
+        0,  1,  v * dt * cos(heading_r);
+            0,  0,  1];
+    B = [dt * cos(heading_r),    0;
+     dt * sin(heading_r),    0;
+     dt * tan(heading_r)/L,  v*dt/(L * cos(delta_r)^2)];
+    """
+
+
+
+    K = calcu_K(A,B,Q,R)
+
+    u = -K @ X
+    v_delta = u[0]
+    Delta_delta = pi_2_pi(u[1])
+    return v_delta,Delta_delta,delta_r,latError
+
+def update(x, y, yaw, v, v_delta,Delta_delta,dt,L,refSpeed,refDelta):
+    Delta = refDelta + Delta_delta
+    x = x + v * math.cos(yaw) * dt
+    y = y + v * math.sin(yaw) * dt
+    yaw = yaw + v / L * math.tan(Delta) * dt
+    v = v + v_delta
+    return x, y, yaw, v, Delta
 
 def main():
     ax = [0.0, 6.0, 12.5, 10.0, 17.5, 20.0, 25.0]
     ay = [0.0, -3.0, -5.0, 6.5, 3.0, 0.0, 0.0]
     goal = [ax[-1], ay[-1]]
-    cx, cy, cyaw, ck, s = cubic_spline_planner.calc_spline_course(
+    refPos_x, refPos_y, refHeading, refK, s = cubic_spline_planner.calc_spline_course(
         ax, ay, ds=0.1)
-    dt = 0.1;
-    L = 2.9;
-    Q = 100 * np.eye(3);
-    R = np.eye(2) * 2;
-    refPos_Delta = math.atan(L * s[0]);
+    dt = 0.1
+    L = 2.9
+    Q =  np.eye(3)
+    R = np.eye(2)
+    refPos_Delta = []
+    for i in range(len(refK)):
+        refPos_Delta.append(L*refK[i])
 
-    refSpeed = 40 / 3.6;
+    refSpeed = 40 / 3.6
 
-    x = ax[0] + 0.5;
-    y = ay[1] + 0.5;
-    yaw = cyaw[1] + 0.02;
-    v = 10;
-    Delta = 0;
-    idx = 0;
-    pos_actual = [x, y];
-    v_actual = v;
-    Delta_actual = Delta;
-    idx_actual = 1;
-    latError_LQR = [];
+    x = ax[0] + 0.5
+    y = ay[1] + 0.5
+    yaw = refHeading[1] + 0.02
+    v = 0
+    Delta = 0
+    idx = 0
+    pos_actual_x = [x]
+    pos_actual_y = [y]
+    v_actual = v
+    Delta_actual = Delta
+    idx_actual = 1
+    latError_LQR = []
+    # Q = np.ones((3, 3))
+    # R = np.ones((1, 1))
 
-    while idx < len(cx) - 1:
-        idx =
+    while idx < len(refPos_x) - 1:
+        idx = calc_target_index(x, y, refPos_x, refPos_y)
+
+        v_delta, delta, delta_r, latError = LQR_contorl(idx,x,y,v,yaw,refPos_x,refPos_y,refHeading,refPos_Delta,refK,L,Q,R,dt)
+
+        if abs(latError > 3):
+            print("误差过大,跳出程序")
+            break
+        x,y,yaw,v,Delta = update(x,y,yaw,v, v_delta,delta, dt,L, refSpeed,delta_r)
+        plt.plot(refPos_x, refPos_y, "-r")
+        pos_actual_x.append(x)
+        pos_actual_y.append(y)
+        plt.plot(pos_actual_x, pos_actual_y,"ob",)
+        plt.pause(0.0001)
+        # draw.draw_car(x, y, yaw, Delta)
 
 
-    plt.plot(cx, cy)
+    # plt.plot(pos_actual_x, pos_actual_y)
     plt.show()
 if __name__ == '__main__':
     main()
